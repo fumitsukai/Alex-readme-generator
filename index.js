6cb0dd44ffa4da0169c1a5548616649e095f949b
fumitsukai <pandeleaalexandru321@gmail.com>
2024-02-02 19:47:41 +0000

    added questions and function to write to file

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "Enter Project Name: ",
+    "Type a short description of the project: ",
+    "How to install and Run the Project? ",
+    "How do you use the project? ",
+    "Choose a licence: ",
+    "How to contribute?"
 ];
+
+const [title, description, install, usage, licence, contribution] = questions;
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName,generateMarkdown(data), err => {
+        err ? console.log(err) : console.log("succcess");
+    })
 }
 
 // function to initialize program
